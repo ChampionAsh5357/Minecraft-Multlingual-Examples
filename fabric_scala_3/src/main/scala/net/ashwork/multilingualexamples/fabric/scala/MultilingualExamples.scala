508509e7bfdd9f0c9b260e77f64383ceb7375261
ChampionAsh5357 <championash5357@gmail.com>
2021-09-13 09:11:56 -0400

    Add scala 3 to example start

---
+++ fabric_scala_3/src/main/scala/net/ashwork/multilingualexamples/fabric/scala/MultilingualExamples.scala
@@ -0,0 +1,30 @@
+/*
+ * Multilingual Examples
+ * Written 2021-2021 by ChampionAsh5357.
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related
+ * and neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this
+ * software. If not, see https://creativecommons.org/publicdomain/zero/1.0/.
+ */
+
+package net.ashwork.multilingualexamples.fabric.scala
+
+import net.fabricmc.api.ModInitializer
+
+/**
+ * The main mod class. This is where the initialization of the mod happens.
+ * The fully qualified name of this class must match that within [[entrypoints.main]].
+ * Any entry point must implement [[ModInitializer]]
+ */
+final class MultilingualExamples extends ModInitializer {
+
+    /**
+     * Initializes the called data once Minecraft is considered to be in a
+     * mod-load-ready state.
+     */
+    override def onInitialize(): Unit = {
+    }
+}
