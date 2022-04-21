44eca34f45cdd2a6d674394867fda9fd72e6d36c
ChampionAsh5357 <championash5357@gmail.com>
2022-04-21 13:32:43 -0400

    Update all licenses

--- fabric/fabric-java/src/main/java/net/ashwork/mc/multilingualexamples/client/MultilingualExamplesClient.java
+++ fabric/fabric-java/src/main/java/net/ashwork/mc/multilingualexamples/client/MultilingualExamplesClient.java
@@ -1,13 +1,7 @@
 /*
  * Multilingual Examples
- * Written 2021-2022 by ChampionAsh5357.
- *
- * To the extent possible under law, the author(s) have dedicated all copyright and related
- * and neighboring rights to this software to the public domain worldwide. This software is
- * distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along with this
- * software. If not, see https://creativecommons.org/publicdomain/zero/1.0/.
+ * Written 2021-2022 by ChampionAsh5357
+ * SPDX-License-Identifier: CC0-1.0
  */
 
 package net.ashwork.mc.multilingualexamples.client;
