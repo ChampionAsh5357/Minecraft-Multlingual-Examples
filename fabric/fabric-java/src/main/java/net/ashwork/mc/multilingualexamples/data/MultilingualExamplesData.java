f2fa939c229e72d1fcb237bcd8b6d65927ff2f77
ChampionAsh5357 <championash5357@gmail.com>
2022-10-11 22:44:36 -0400

    Add datagen for fabric java

---
+++ fabric/fabric-java/src/main/java/net/ashwork/mc/multilingualexamples/data/MultilingualExamplesData.java
@@ -0,0 +1,20 @@
+package net.ashwork.mc.multilingualexamples.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+/**
+ * A class instance used to run providers to generate files necessary by specified
+ * objects (e.g. models, localizations, etc.) The fully qualified name of this class
+ * must match that within {@code entrypoints.fabric-datagen}. Any data entry point
+ * must implement {@link DataGeneratorEntrypoint}.
+ */
+public final class MultilingualExamplesData implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(final FabricDataGenerator gen) {
+        // Add providers
+        gen.addProvider(Localizations::new);
+        gen.addProvider(Models::new);
+    }
+}
