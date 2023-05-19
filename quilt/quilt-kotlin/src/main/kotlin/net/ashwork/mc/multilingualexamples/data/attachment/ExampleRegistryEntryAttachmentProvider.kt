44353f6cdf7bb5dfd9fe483921d94d794ddc2fdc
ChampionAsh5357 <championash5357@gmail.com>
2023-05-19 13:32:28 -0400

    Update licenses

--- quilt/quilt-kotlin/src/main/kotlin/net/ashwork/mc/multilingualexamples/data/attachment/ExampleRegistryEntryAttachmentProvider.kt
+++ quilt/quilt-kotlin/src/main/kotlin/net/ashwork/mc/multilingualexamples/data/attachment/ExampleRegistryEntryAttachmentProvider.kt
@@ -1,3 +1,9 @@
+/*
+ * Multilingual Examples
+ * Written 2021-2023 by ChampionAsh5357
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
 package net.ashwork.mc.multilingualexamples.data.attachment
 
 import net.ashwork.mc.multilingualexamples.registrar.SQUISHED_WAFFLE
